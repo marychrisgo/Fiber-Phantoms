9a7f16704a5386e54610f72ef947ce6c0ed082c8
marychrisgo <marychrisgo@gmail.com>
2024-09-12 10:22:09 +0200

    Various changes:
    1. Add the concept of resin attenuation
    2. Add radius parameter in paramaters.json

--- Fiber-phantom-module/fiber_phantom/generate_filaments.py
+++ Fiber-phantom-module/fiber_phantom/generate_filaments.py
@@ -9,8 +9,13 @@ import nibabel as nib
 
 # Define the attenuation coefficients
 ATTENUATION_AIR = 0.0
-ATTENUATION_BACKGROUND = 0.0025
+ATTENUATION_RESIN = 0.1
 ATTENUATION_FIBER = 1.0
+
+# # refractive indices
+# RI_AIR = 
+# RI_RESIN = 
+# RI_FIBER = 
 
 # Slicer reads .nii
 def save_as_nifti(array, file_path):
@@ -54,15 +59,21 @@ def update_volume_with_filament(volume, filament, radius=1, pipe_radius=50, inte
 
 ############################################################
 
+def fill_pipe_with_resin(volume, pipe_radius=50):
+    center_y, center_z = volume.shape[1] // 2, volume.shape[2] // 2
+    for x in range(volume.shape[0]):
+        for y in range(volume.shape[1]):
+            for z in range(volume.shape[2]):
+                if volume[x, y, z] == 0.0:  # unfilled spaced
+                    if is_within_pipe((x, y, z), center_y, center_z, pipe_radius):
+                        volume[x, y, z] = ATTENUATION_RESIN  
 
-def generate_and_count_filaments(volume, num_filaments, generator, defect_generator, pipe_radius=50, min_length=512, max_length=512, radius=4, bias=0.90, preferred_direction=[1,0,0]):
+
+def generate_and_count_filaments(volume, num_filaments, generator, defect_generator, pipe_radius=50, min_length=512, max_length=512, radius=5, bias=0.90, preferred_direction=[1, 0, 0]):
     successful_filaments = 0
     filaments = []
     total_attempts = 0
-    max_total_attempts = 10000
-
-    # Initialize volume with background attenuation coefficient
-    volume.fill(ATTENUATION_BACKGROUND)
+    max_total_attempts = 1000
 
     while successful_filaments < num_filaments and total_attempts < max_total_attempts:
         filament = generate_3d_filament(volume, generator, min_length, max_length, radius, pipe_radius, bias, preferred_direction)
@@ -75,9 +86,13 @@ def generate_and_count_filaments(volume, num_filaments, generator, defect_genera
     if successful_filaments < num_filaments:
         print(f"Warning: Only able to place {successful_filaments} filaments after {total_attempts} attempts.")
 
+    # Fill inside the pipe with resin where it's still air
+    fill_pipe_with_resin(volume, pipe_radius)
+
     # Add voids (air) after placing filaments
     volume = defect_generator.apply(volume)
     return successful_filaments, filaments
+
 
 def generate_3d_filament(volume, generator, min_length=512, max_length=512, radius=6, pipe_radius=50, bias=0.90, preferred_direction=[1, 0, 0]):
     # starting point without validation
